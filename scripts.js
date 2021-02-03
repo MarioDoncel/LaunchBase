b10c54aebd5e376773e3c8d6c0d1e3290560533c
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-02-03 13:27:43 -0300

    Desafio construindo Foodfy finalizado =)git add .

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const cards = document.querySelectorAll(".card")
+const modal = document.querySelector(".modal-overlay")
+const modalContent = document.querySelector(".modal-content")
+
+
+cards.forEach(card => {
+    let image = card.querySelector(".image-container")
+    let recipe = card.querySelector(".recipe-name")
+    let author = card.querySelector(".author")
+
+    card.addEventListener("click", () => {
+        modal.classList.add('active')
+        modalContent.innerHTML += `
+                <div class="image-container">
+                    ${image.innerHTML}
+                </div>
+                <div class="recipe-name">
+                    ${recipe.innerHTML}
+                </div>
+                <div class="author">
+                    ${author.innerHTML}
+                </div>   `
+    })
+});
+
+document.querySelector(".close-modal").addEventListener("click", () => {
+    modal.classList.remove('active')
+    modalContent.innerHTML = ""
+})
+
