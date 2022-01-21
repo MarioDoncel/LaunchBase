904ea1c09fd157b4816948ab63556bafd72fcdd1
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2022-01-21 16:48:43 -0300

    fix: env host

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg')
 module.exports = new Pool({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
-    host: process.env.DATABASE_URL,
+    host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE
 })
